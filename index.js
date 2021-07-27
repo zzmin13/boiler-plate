249c460e87478b0893b755e4c089459b2a7a5526
zzmin13 <zzzz1997@naver.com>
2021-07-27 16:39:21 +0900

    mongoDB 연결 및 User Model 작성

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = express();
 const port = 5000;
+
+mongoose
+  .connect(process.env.REACT_APP_MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log(`⭕ MongoDB Connected`))
+  .catch((error) => console.log(error));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
