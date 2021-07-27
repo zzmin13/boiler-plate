b40e2eb6ca07d8b643d591520991338abb516c42
zzmin13 <zzzz1997@naver.com>
2021-07-27 17:49:28 +0900

    /register 구현

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { User } = require("./models/User");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 const port = 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 mongoose
   .connect(process.env.REACT_APP_MONGO_URL, {
@@ -18,6 +22,16 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  // 회원 가입할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -31,5 +31,5 @@ const userSchema = mongoose.Schema({
   },
 });
 
-const User = mongoose.Model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = { User };
